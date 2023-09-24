e40474ca65bf4108afb60142c9678e4073c3b627
abhinavbgpgit <abhinavbgp@gmail.com>
2023-09-24 16:57:52 +0530

    hmm

--- src/pages/home/TestPreparation.js
+++ src/pages/home/TestPreparation.js
@@ -5,24 +5,24 @@ const TestPreparation = () => {
   return (
     <div>
        <div className="testPreparation_main">
-
-<div className="carousel_left">
-  <div className="carousel_left_box">
+       <div className="carousel_right">
+  <img className="test__praparation_img" src={test_content} alt="test1" />
+</div>
+<div className="testPreparation_left">
+  <div className="testPreparation_left_box">
 <div className="what_you_will_get">Ace your tests to build confidence for your exams</div>
-<div className="carousel_heading">Take your Test Preparation to the next level</div>
+<div className="testPreparation_heading">Take your Test Preparation to the next level</div>
 <div className="line"></div>
-<div className="carousel_description">
+<div className="testPreparation_description">
 Our Online tests make you feel more confident because you can take them in a comfortable setting, get instant feedback on how you're doing, and they adjust to your level, so you're not overwhelmed. This helps you believe in your abilities and get better at what you're learning.
 </div>
-<div className="carousel_button">Try For Free </div>
+<div className="testPreparation_button">Try For Free </div>
 
   </div>
 
 
 </div>
-<div className="carousel_right">
-  <img className="test1" src={test_content} alt="test1" />
-</div>
+
 </div> 
     </div>
   )
