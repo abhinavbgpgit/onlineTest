7be5d3a6e6e4d2a29c618122944b39e9adc6d9de
abhinavbgpgit <abhinavbgp@gmail.com>
2023-09-27 14:29:09 +0530

    footer ready

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,52 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import playStoreIcon from "../../images/googleplay.png";
 import { BsFacebook, BsYoutube, BsWhatsapp } from "react-icons/bs";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import "./footer.css";
 import { AiOutlineCaretDown } from "react-icons/ai";
 const Footer = () => {
+  const [toggle, setToggle]=useState(false)
   return (
     <div className="text-white">
-      <div className="bg-[#75b4df] px-[230px] py-4 flex items-center ">
+      <div className="bg-[#75b4df] px-[230px] py-4 flex items-center " onClick={()=>setToggle(!toggle)}>
         <AiOutlineCaretDown className="cursor-pointer" />
         <span className="text-[18px] ml-2 font-semibold text-blue-700 hover:text-blue-950 cursor-pointer ">
           Quick Links{" "}
         </span>
-        {/* <span className="text-base">Explore popular categories</span> */}
+        <span className="text-base mx-5 text-gray-900">Explore popular categories</span>
       </div>
-      
-      <div className="bg-[#092C43] pl-[230px] pr-[500px] py-10 flex items-center justify-between">
-        <div className="text-[17px]">
-          <h1 className="font-bold pb-4">IIt JEE Advanced</h1>
-          <p>IIT JEE Advanced Preparation Courses</p>
-          <p>IIT JEE Advanced Examination</p>
-          <p>IIT JEE Advanced Mock Tests</p>
-        </div>
+      {toggle && (
+ <div className="bg-[#ebf1f5] pl-[230px] pr-[500px] py-10 flex items-center justify-between text-black slow_motion">
+ <div className="text-[17px]">
+   <h1 className="font-bold pb-4">IIT JEE Advanced</h1>
+   <p>IIT JEE Test Chaperwise</p>
+   <p>IIT JEE Test Full Syllabus </p>
 
-        <div className="text-[17px]">
-          <h1 className="font-bold pb-4">JEE Advanced</h1>
-          <p>JEE Main Preparation Courses</p>
-          <p>JEE Main Examination</p>
-          <p>JEE Main Test Series</p>
-        </div>       
+ </div>
 
-        
-        <div className="text-[17px]">
-          <h1 className="font-bold pb-4">NEET-UG</h1>
-          <p>NEET-UG Preparation Courses</p>
-          <p>NEET Crash Course</p>
-          <p>NEET Online Test Series</p>
-        </div>       
-      </div>
+ <div className="text-[17px]">
+   <h1 className="font-bold pb-4">JEE Advanced</h1>
+   <p>JEE Main Test Chaperwise</p>
+   <p className="">JEE Main Test Full Syllabus</p>  
+ </div>       
 
-      <div className="bg-[#173C5A] px-[230px] py-10 flex items-center justify-between">
-        <div className="text-[17px]">
-          <h1 className="font-bold pb-4">MORE</h1>
-          <p>Know About us</p>
-          <p>Testinomials</p>
-        </div>
+ 
+ <div className="text-[17px]">
+   <h1 className="font-bold pb-4">NEET-UG</h1>
+   <p>NEET Test Chaperwise</p>
+   <p>NEET Test Full Syllabus</p>
 
-        <div className="text-[17px]">
+ </div>       
+</div>
+
+      )}
+     
+
+      <div className="bg-[#296f9e] px-[230px] py-5 flex items-center justify-between text-white">
+        <div className="text-[17px] flex gap-10">
+          <div>Know About us</div>
+          <div>Testinomials</div>
+       
           <div>Contact Us</div>
           <div>Sitemap</div>
         </div>
@@ -59,7 +59,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="bg-[#092C43] py-4 px-[230px] flex items-center justify-between">
+      <div className="bg-[#75b4df] py-4 px-[230px] flex items-center justify-between text-blue-950">
         <div className="flex items-center justify-center gap-3 text-base">
           <div>Privacy</div>
           <div>|</div>
